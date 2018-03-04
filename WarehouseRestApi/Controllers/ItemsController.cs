2e60ebb0f6e21d3490d0dbf687af2c849c9c6dc3
peltzergergely <peltzer.gergely@gmail.com>
2018-03-04 18:06:55 +0100

    Working Warehouse Api
    Please read the summary in Startup.cs and create the datatable in the SQL SERVER OBJECT EXPLORER inside Visual Studio

---
+++ WarehouseRestApi/Controllers/ItemsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Belgrade.SqlClient;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace WarehouseRestApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Items")]
+    public class ItemsController : Controller
+    {
+        private readonly IQueryPipe SqlPipe;
+        private readonly ICommand SqlCommand;
+
+        public ItemsController(ICommand sqlCommand, IQueryPipe sqlPipe)
+        {
+            this.SqlCommand = sqlCommand;
+            this.SqlPipe = sqlPipe;
+        }
+
+        // GET: api/Items
+        [HttpGet]
+        public async Task Get()
+        {
+            await SqlPipe.Stream("select * from Items FOR JSON PATH", Response.Body, "[]");
+        }
+
+        // GET: api/Items/5
+        [HttpGet("{id}")]
+        public async Task Get(int id)
+        {
+            var cmd = new SqlCommand("select * from Items where Id = @id FOR JSON PATH, WITHOUT_ARRAY_WRAPPER");
+            cmd.Parameters.AddWithValue("id", id);
+            await SqlPipe.Stream(cmd, Response.Body, "{}");
+        }
+
+        // POST: api/Items
+        [HttpPost]
+        public async Task Post()
+        {
+            string items = new StreamReader(Request.Body).ReadToEnd();
+            var cmd = new SqlCommand(
+                                    @"insert into Items
+                                        select *
+                                        from OPENJSON(@items)
+                                        WITH(Name nvarchar(200), Owner nvarchar(100), Pos int)");
+            cmd.Parameters.AddWithValue("Items", items);
+            await SqlCommand.Exec(cmd);
+        }
+
+        // PUT: api/Items/5
+        [HttpPut("{id}")]
+        public async Task Put(int id)
+        {
+            string items = new StreamReader(Request.Body).ReadToEnd();
+            var cmd = new SqlCommand(
+                                    @"update Items
+                                        set Name = json.Name,
+                                        Owner = json.Owner,
+                                        Pos = json.Pos
+                                    from OPENJSON( @items )
+                                         WITH(Name nvarchar(200), Owner nvarchar(100),
+                                            Pos int) AS json
+                                        where Id = @id");
+            cmd.Parameters.AddWithValue("id", id);
+            cmd.Parameters.AddWithValue("items", items);
+            await SqlCommand.Exec(cmd);
+        }
+
+        // PATCH api/Todo
+        [HttpPatch]
+        public async Task Patch(int id)
+        {
+            string items = new StreamReader(Request.Body).ReadToEnd();
+            var cmd = new SqlCommand(
+                                @"update Items
+                                     set Name = ISNULL(json.Name, Name),
+                                     Owner = ISNULL(json.Owner, Owner),
+                                     Pos = ISNULL(json.Pos, Pos)
+                                from OPENJSON(@items)
+                                WITH(Name nvarchar(200), 
+                                     Owner nvarchar(100),
+                                     Pos int) AS json
+                                where Id = @id
+                                ");
+            cmd.Parameters.AddWithValue("id", id);
+            cmd.Parameters.AddWithValue("todo", items);
+            await SqlCommand.Exec(cmd);
+        }
+
+        // DELETE api/Todo/5
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            var cmd = new SqlCommand(@"delete Items where Id = @id");
+            cmd.Parameters.AddWithValue("id", id);
+            await SqlCommand.Exec(cmd);
+        }
+    }
+}
--- WarehouseRestApi/Startup.cs
+++ WarehouseRestApi/Startup.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Belgrade.SqlClient;
+using Belgrade.SqlClient.SqlDb;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+
+/// <summary>
+/// to create an executable run from console: dotnet publish -c Release -r win10-x64
+/// also add <RuntimeIdentifiers>win10-x64</RuntimeIdentifiers> to the .csproj file
+/// for the database use the following in T-SQL (obviously change the connstring):
+/// CREATE TABLE [dbo].[Items] (
+///[Id] INT IDENTITY(1, 1) NOT NULL,
+///[Name]  NVARCHAR(200) NOT NULL,
+///[Owner] NVARCHAR(100) NOT NULL,
+///[Pos]   INT NOT NULL,
+///    PRIMARY KEY CLUSTERED([Id] ASC)
+///);
+///
+/// </summary>
 
 namespace WarehouseRestApi
 {
@@ -23,6 +40,13 @@ namespace WarehouseRestApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            const string ConnString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=WarehouseDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            services.AddTransient<IQueryPipe>(_ => new QueryPipe(new SqlConnection(ConnString)));
+            services.AddTransient<ICommand>(_ => new Command(new SqlConnection(ConnString)));
+
+            // Add framework services.
             services.AddMvc();
         }
 
